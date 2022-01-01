77a225bff8528d9fb5402c8ce186c4a542d46ee8
James Dent <semajdent@gmail.com>
2022-01-01 15:13:52 +1300

    Updating styling; added external stylesheet.

--- main.py
+++ main.py
@@ -3,6 +3,19 @@
 import random
 from datetime import datetime
 
+
+class OpenElement:
+    def __init__(self, top, bot):
+        self._top = top
+        self._bot = bot
+
+    def __enter__(self):
+        print(self._top)
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        print(self._bot)
+
+
 width = 5
 height = 5
 dimesions = width, height
@@ -27,7 +40,7 @@ def starting_player():
 def code_generator():
     cards = ['<td class=\"table-danger\">'+ '<p class="text-center">' + p1_token +'</p>'] * p1 + ['<td class=\"table-primary\">' + '<p class="text-center">' + p2_token+ '</p>'] * p2 + ['<td class=\"table-dark\">' + '<p class="text-center">' + '<b>O</b>' + '</p>'] * omicorn + ['<td class=\"table-info\">' + '<p class="text-center">' + '\N{Face with Medical Mask}'+'</p>'] * null
     random.shuffle(cards)
-    output = "<table class=\"table\">\n" + "<tbody>\n"
+    output = "<table id=\"content\" class=\"table\">\n" + "<tbody>\n"
     for i in range(0, len(cards), width):
         output += "<tr>\n"
         for j in range(width):
@@ -37,5 +50,6 @@ def code_generator():
     output += "</tbody>\n" + "</table>\n"
     print(output)
     # print('<br/>')
-    starting_player()
-    compile_time()
+    with OpenElement('<div id="banner-bottom">', '</div>'):
+        starting_player()
+        compile_time()
